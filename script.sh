6932dd65fc67c8965a66582c65ec7060d8700f39
julian1516 <123390515+julian1516@users.noreply.github.com>
2023-01-31 10:47:19 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,8 +4,8 @@
 SECRET=$(oc get secrets/encryption-config -n etcd-secret-backup -o=jsonpath='{.data.encryption-config}' | base64 -d)
 
 # Définition de l'adresse Vault et du token
-VAULT_ADDR=https://10.247.65.21:8200
-VAULT_TOKEN=hvs.CAESII1w3oX4AOIWONl7bzpLc-A36y02SKATmcDauw7Iv5faGigKImh2cy5xYjNUdEhva25GaU5BbHFwVDBNTFJOZTcuS0RKZjAQh-QL
+VAULT_ADDR=
+VAULT_TOKEN=
 
 # Connexion à Vault
 vault login -address=$VAULT_ADDR -tls-skip-verify token=$VAULT_TOKEN
