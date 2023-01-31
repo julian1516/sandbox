93df6301914273d610a916bf74c79482388be5cf
julian1516 <123390515+julian1516@users.noreply.github.com>
2023-01-31 10:42:30 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,11 +3,13 @@
 # Récupération du secret
 SECRET=$(oc get secrets/encryption-config -n etcd-secret-backup -o=jsonpath='{.data.encryption-config}' | base64 -d)
 
-# Définition de l'adresse Vault
+# Définition de l'adresse Vault et du token
 VAULT_ADDR=https://10.247.65.21:8200
+VAULT_TOKEN=hvs.CAESII1w3oX4AOIWONl7bzpLc-A36y02SKATmcDauw7Iv5faGigKImh2cy5xYjNUdEhva25GaU5BbHFwVDBNTFJOZTcuS0RKZjAQh-QL
 
-# login puis Poussez le secret vers Vault
-#curl --request POST --data "$SECRET" "$VAULT_ADDR/v1/secret/data/encryption-config"
+# Connexion à Vault
+vault login -address=$VAULT_ADDR -tls-skip-verify token=$VAULT_TOKEN
 
-vault login -address=https://10.247.65.21:8200 -tls-skip-verify
-VAULT_NAMESPACE=automation vault kv get -tls-skip-verify -address=https://10.247.65.21:8200 kv-v2/etcd-encryption-key
+
+# Poussez le secret vers Vault
+VAULT_NAMESPACE=automation vault kv get -tls-skip-verify -address=$VAULT_ADDR kv-v2/etcd-encryption-key
