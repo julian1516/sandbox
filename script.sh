213d55c2801cbecfca1f7b06c99076bd4a962fa5
julian1516 <123390515+julian1516@users.noreply.github.com>
2023-01-31 10:19:33 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-echo "Vault version: $(vault version)"
-echo "OpenShift version: $(oc version)"
-echo "curl version: $(curl --version | head -n 1)"
-echo "wget version: $(wget --version | head -n 1)"
-echo "unzip version: $(unzip -v | head -n 1)"
+
+# Récupération du secret
+SECRET=$(oc get secrets/encryption-config -n etcd-secret-backup -o=jsonpath='{.data.encryption-config}' | base64 -d)
+
+# Définition de l'adresse Vault
+VAULT_ADDR=https://10.247.65.21:8200
+
+# login puis Poussez le secret vers Vault
+#curl --request POST --data "$SECRET" "$VAULT_ADDR/v1/secret/data/encryption-config"
+
+vault login -address=https://10.247.65.21:8200 -tls-skip-verify
+VAULT_NAMESPACE=automation vault kv get -tls-skip-verify -address=https://10.247.65.21:8200 kv-v2/etcd-encryption-key
